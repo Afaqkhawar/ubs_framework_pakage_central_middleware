e4437be4eca47cd33a4415d28e5c5db0ebe43378
Afaq Khawar <209493849+Afaqkhawar@users.noreply.github.com>
2025-08-12 13:08:07 +0500

    cutomize package for testing

--- index.js
+++ index.js
@@ -1,5 +1,12 @@
-// index.js
-export { getServerResponse } from './src/Common/getServerResponse.js';
-export { default as serverCommunicationHelper } from './src/Common/serverCommunicationHelper.js';
-export { showSuccessToast, showErrorToast, showInfoToast } from './src/Common/ToastUtils.js';
+// index.js (CJS entry)
+const { getServerResponse } = require('./src/Common/getServerResponse.js');
+const serverCommunicationHelper = require('./src/Common/serverCommunicationHelper.js');
+const { showSuccessToast, showErrorToast, showInfoToast } = require('./src/Common/ToastUtils.js');
 
+module.exports = {
+  getServerResponse,
+  serverCommunicationHelper,
+  showSuccessToast,
+  showErrorToast,
+  showInfoToast
+};
